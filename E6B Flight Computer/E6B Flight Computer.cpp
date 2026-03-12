a449fbcaede229bbc3f899d69454d00e5f802be5
CaptainKloudy <edjmcfarl06@gmail.com>
2026-03-12 19:30:17 -0400

    As the first commit for this project, I added the first line of code besides "Hello World"! Can't wait to see where this project goes!

--- E6B Flight Computer/E6B Flight Computer.cpp
+++ E6B Flight Computer/E6B Flight Computer.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+	cout << "This is a simple E6B Flight Computer program.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
