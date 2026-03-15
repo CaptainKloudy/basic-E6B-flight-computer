9ceacd43e75e6389f801ac155fc998a16f8aba09
CaptainKloudy <edjmcfarl06@gmail.com>
2026-03-15 17:03:10 -0400

    I added a menu in main and created a options list for the user.

--- E6B Flight Computer/E6B Flight Computer.cpp
+++ E6B Flight Computer/E6B Flight Computer.cpp
@@ -8,47 +8,70 @@ using namespace std;
 //Given the time and speed
 //Distance formula: sqrt(x-x)^2+(y-y)^2
 
-int calcFunc(int distance, double speed)
+double calcFunc(double distance, double speed)
 {
-	int time = (distance / speed) * 60;
+	double time = (distance / speed) * 60;
 	return time;
 }
 
-//Whenever you are using variables from another function in main, make sure the variables match up in the main fucntion when it's delcalred
-double speedTimeDist(double time, double speed)
- {
-	double distance;
-	return time * speed;
- }
-
-int main()
+void speedTimeDistance()
 {
-	cout << "E6B Flight Computer Test Program.\n";
 	int speed, dist, timeMin;
 	int timeHr;
 
+	// Need to add another menu here to see what they want to calculate. Ask if they want to find distance, speed or time
 
-	//Get Data from the User
-
-	cout << "Distance Traveled in NM: ";
+	cout << "What is your distance Traveled in NAUTICAL MILES?: ";
 	cin >> dist;
 
-	cout << "Ground Speed in KTS: ";
+	cout << "What is your ground speed in KNOTS?: ";
 	cin >> speed;
-
-	//Made sure the function could only call 2 variables
-	//result = speedTimeDist(s, t);
-
-	//When calling functions make sure that the variables are called in order of need
-	timeMin = calcFunc(dist, speed);
-
-	cout << "Total Time is: " << timeMin << " minutes" << endl;
 	
-	//Also make the time return in a hour format
+	timeMin = calcFunc(dist, speed);
+	cout << " Your Total Time is: " << timeMin << " minutes" << endl;
+
 	timeHr = timeMin / 60;
-	cout << "Total Time is: " << timeHr << " hours and " << timeMin%60 << " minutes." << endl;
+
+	cout << "Total Time is: " << timeHr << " hours and " << timeMin % 60 << " minutes and " << 60/timeMin << " seconds" << endl;
+	
 
 
-	return 0;
+}
+
+//Whenever you are using variables from another function in main, make sure the variables match up in the main fucntion when it's delcalred
+//double speedTimeDist(double time, double speed)
+ // {
+	//double distance;
+	//return time * speed;
+ //}
+
+int main()
+{
+
+	int choice;
+	
+	cout << "===Welcome to the E6B Flight Computer!===\n";
+	cout << " This was programed by Saleema and Ethan!   \n";
+	cout << "\n";
+	cout << "How can our program assist you today?\n";
+	cout << "\n";
+	cout << "Please select from the options below\n";
+	cout << "\n";
+	cout << "1 - Speed, Time & Distance Calculations\n";
+	cout << "\n";
+	cout << "2 - Fuel Calculations\n";
+	cout << "\n";
+	cout << "Enter your choice: ";
+	cin >> choice;
+	
+	switch (choice)
+	{
+	case 1:
+		cout << "You have selected Option 1.\n";
+		speedTimeDistance();
+		break;
+	case 2:
+		cout << "You have selected Option 2.";
+	}
 
 }
